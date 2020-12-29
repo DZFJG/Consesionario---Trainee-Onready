a5aa4e56480709b44b602d6e04df353c90de40de
FelipeJG <73566196+DZFJG@users.noreply.github.com>
2020-12-29 14:32:54 -0500

    Formato precio

--- Concesionario - Ejercicio Trainee Onready/src/Beans/Automovil.java
+++ Concesionario - Ejercicio Trainee Onready/src/Beans/Automovil.java
@@ -18,6 +18,6 @@ public class Automovil extends Vehiculo {
 	
 	@Override
 	public String toString() {
-		return "Marca: " + getMarca() + " // Modelo: " + getModelo() + " // Puertas: " + getPuertas() + " // Precio: $" + getPrecio();
+		return "Marca: " + getMarca() + " // Modelo: " + getModelo() + " // Puertas: " + getPuertas() + "cc // Precio: " + getFormatoPrecio().format(getPrecio());
 	}
 }
